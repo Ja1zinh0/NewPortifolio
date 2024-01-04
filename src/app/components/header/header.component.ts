071df2f8e4817b20ceefa96157c7421bfd3ba17d
Ja1zinh0 <joao2011bha@hotmail.com>
2024-01-04 19:47:00 -0300

    change: header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
+  constructor() {}
+  menu_icon_variable: boolean = false;
+  menuVariable: boolean = false;
+  ngOnInit(): void {}
+  openMenu() {
+    this.menuVariable = !this.menuVariable;
+    this.menu_icon_variable = !this.menu_icon_variable;
+  }
 }
