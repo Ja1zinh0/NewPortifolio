b21058e2bd5226f9fb423c4164d2a3c3b2620173
Ja1zinh0 <joao2011bha@hotmail.com>
2024-01-17 16:21:10 -0300

    new html organization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { BodyComponent } from './components/body/body.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
   ],
   imports: [
     BrowserModule,
