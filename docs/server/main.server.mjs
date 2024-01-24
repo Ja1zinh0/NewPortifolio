eb3779f976aca5e3305900242f721e46706545ec
Ja1zinh0 <joao2011bha@hotmail.com>
2024-01-24 18:07:00 -0300

    teste2

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-DEY3PCDE.mjs";import"./chunk-PBORCN57.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-3FOMK24J.mjs";import"./chunk-PBORCN57.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
